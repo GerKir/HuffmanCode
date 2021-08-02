48ca06fa413d69511a14e2c910c9240e43bf4f81
Kirill Gerasimov <gerasimovkd@gmail.com>
2021-08-02 19:34:02 +0300

    Rewritten queue structure from sorted array to min heap

--- src/main/java/HuffmanQueue.java
+++ src/main/java/HuffmanQueue.java
@@ -7,42 +7,118 @@ public class HuffmanQueue<E> {
 		this.queue = new Object[DEFAULT_CAPACITY];
 	}
 
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			sb.append("[")
+					.append(i)
+					.append("] ")
+					.append(queue[i])
+					.append("\n");
+		}
+		return sb.toString();
+	}
+
 	public void push(E value, int weight) {
+		if (size >= queue.length) {
+			throw new IllegalStateException("Queue is full");
+		}
 		HuffmanQueueEntry<E> entry = new HuffmanQueueEntry<>(value, weight);
 		push(entry);
 	}
 
-	public void push(HuffmanQueueEntry<E> entry) {
-		for (int elementIndex = 0; elementIndex < size; elementIndex++) {
-			HuffmanQueueEntry<E> element = getElement(elementIndex);
-			if (entry.getWeight() < element.getWeight()) {
-				queue[elementIndex] = entry;
-				push(element);
-				return;
-			}
-		}
+	private synchronized void push(HuffmanQueueEntry<E> entry) {
 		queue[size++] = entry;
+		if (size == 1) {
+			return;
+		}
+
+		int childNumber = size;
+		int parentNumber = getParentNumber(size);
+		while (shouldSwap(childNumber, parentNumber)) {
+			swapElements(childNumber, parentNumber);
+			childNumber = parentNumber;
+			parentNumber = getParentNumber(childNumber);
+		}
 	}
 
-	private HuffmanQueueEntry<E> getElement(int index) {
-		return ((HuffmanQueueEntry<E>) queue[index]);
-	}
-
-	public E pop() {
+	public synchronized E pop() {
 		if (size == 0) {
-			return null;
+			throw new IllegalStateException("Queue is empty");
 		}
-		HuffmanQueueEntry<E> entity = (HuffmanQueueEntry<E>) queue[0];
-		for (int i = 0; i < size; i++) {
-			queue[i] = queue[i + 1];
+		
+		HuffmanQueueEntry<E> root = (HuffmanQueueEntry<E>) queue[0];
+		HuffmanQueueEntry<E> lastElement = (HuffmanQueueEntry<E>) queue[--size];
+		
+		queue[0] = lastElement;
+		queue[size] = null;
+		
+//		todo: refactoring
+		int parent = 1;
+		int leftChild = getLeftChild(parent);
+		int rightChild = getRightChild(parent);
+		int minChildNumber = findMin(leftChild, rightChild);
+		while (shouldSwap(minChildNumber, parent)) {
+			swapElements(minChildNumber, parent);
+			parent = minChildNumber;
+			leftChild = getLeftChild(parent);
+			rightChild = getRightChild(parent);
+			minChildNumber = findMin(leftChild, rightChild);
 		}
-		size--;
-		return entity.getValue();
+		return root.getValue();
 	}
 
-	public void print() {
-		for (int i = 0; i < size; i++) {
-			System.out.println("[" + i + "] " + queue[i]);
+	private HuffmanQueueEntry<E> getElement(int elementNumber) {
+		return ((HuffmanQueueEntry<E>) queue[--elementNumber]);
+	}
+
+	private int getParentNumber(int childNumber) {
+		return childNumber / 2;
+	}
+
+	private int getLeftChild(int parentNumber) {
+		return parentNumber * 2;
+	}
+
+	private int getRightChild(int parentNumber) {
+		return (parentNumber * 2) + 1;
+	}
+
+	private boolean shouldSwap(int childNumber, int parentNumber) {
+		if (childNumber == 1) {
+			return false;
 		}
+		int childWeight = getElement(childNumber).getWeight();
+		int parentWeight = getElement(parentNumber).getWeight();
+		return childWeight < parentWeight;
+	}
+
+	private boolean shouldSwap(int leftChildNumber, int rightChildNumber, int parentNumber) {
+		int min = findMin(leftChildNumber, rightChildNumber);
+		return shouldSwap(min, parentNumber);
+	}
+
+	private int findMin(int leftChild, int rightChild) {
+		HuffmanQueueEntry<E> left = getElement(leftChild);
+		if (left == null) {
+			return getParentNumber(leftChild);
+		}
+		HuffmanQueueEntry<E> right = getElement(rightChild);
+		if (right == null) {
+			return leftChild;
+		}
+		int compareResult = Integer.compare(left.getWeight(), right.getWeight());
+		return compareResult > 0
+				? rightChild
+				: leftChild;
+	}
+
+	private void swapElements(int newElementNumber, int parentNumber) {
+		HuffmanQueueEntry<E> newElement = getElement(newElementNumber);
+		HuffmanQueueEntry<E> parent = getElement(parentNumber);
+		queue[--newElementNumber] = parent;
+		queue[--parentNumber] = newElement;
 	}
 }
--- src/test/java/HuffmanQueueTest.java
+++ src/test/java/HuffmanQueueTest.java
@@ -1,57 +1,89 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HuffmanQueueTest {
 	
 	@Test
 	public void test() {
 		HuffmanQueue<String> queue = new HuffmanQueue<>();
 		queue.push("1", 1);
+		System.out.println(queue);
 		queue.push("3", 3);
+		System.out.println(queue);
 		queue.push("2", 2);
+		System.out.println(queue);
 		queue.push("4", 4);
+		System.out.println(queue);
 
 		System.out.println(queue.pop());
+		System.out.println(queue);
 		System.out.println(queue.pop());
+		System.out.println(queue);
 		System.out.println(queue.pop());
+		System.out.println(queue);
 		System.out.println(queue.pop());
+		System.out.println(queue);
 		System.out.println(queue.pop());
+		System.out.println(queue);
 	}
 	@Test
 	public void test2() {
 		HuffmanQueue<String> queue = new HuffmanQueue<>();
 		queue.push("4", 4);
+		System.out.println(queue);
 		queue.push("3", 3);
+		System.out.println(queue);
 		queue.push("2", 2);
+		System.out.println(queue);
 		queue.push("1", 1);
-		
-		queue.print();
+		System.out.println(queue);
 	}
 	@Test
 	public void test3() {
 		HuffmanQueue<String> queue = new HuffmanQueue<>();
 		queue.push("1500", 1500);
+		System.out.println(queue);
 		queue.push("3", 3);
+		System.out.println(queue);
 		queue.push("17-1", 17);
+		System.out.println(queue);
 		queue.push("1123", 1123);
+		System.out.println(queue);
 		queue.push("21", 21);
+		System.out.println(queue);
 		queue.push("1-1", 1);
+		System.out.println(queue);
 		queue.push("25", 25);
+		System.out.println(queue);
 		queue.push("1-2", 1);
+		System.out.println(queue);
 		queue.push("17-2", 17);
+		System.out.println(queue);
 		queue.push("41", 41);
+		System.out.println(queue);
 		queue.push("133", 133);
+		System.out.println(queue);
 		queue.push("1-3", 1);
-		
-		queue.print();
+		System.out.println(queue);
 
+		System.out.println("-----------");
+		
+		System.out.println(queue);
 		for (int i = 0; i < 15; i++) {
 			System.out.println(queue.pop());
-			queue.print();
+			System.out.println(queue);
 		}
-		
-		queue.print();
+		System.out.println(queue);
+	}
+	
+	@Test
+	public void test4() {
+		int i = 100;
+		asd(i);
+		System.out.println(i);
+	}
+	
+	private void asd(int i) {
+		i = i + 1;
 	}
 
 }
